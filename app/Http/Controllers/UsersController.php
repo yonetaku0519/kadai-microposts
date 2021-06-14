b8f472f4e6498ad283f4ad83ff7f34eba374726e
yoneyone.takutaku <yonetakuyonetaku@test.jp>
2021-06-14 01:59:31 +0000

    make it better

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,14 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\User;
+
 class UsersController extends Controller
 {
     public function index() 
     {
         
-        $users = User::orderBy('id','desc')->paginate(10);
-        
-        return view('users.index',['users=>$users']);
+        // ユーザ一覧をidの降順で取得
+        $users = User::orderBy('id', 'desc')->paginate(10);
+
+        // ユーザ一覧ビューでそれを表示
+        return view('users.index', [
+            'users' => $users,
+        ]);
         
     }
     
